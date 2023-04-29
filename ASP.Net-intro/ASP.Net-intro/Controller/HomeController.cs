91253edb8c96c9261626bd553a09bf0bc83babbd
jafarova1 <gultajvj@code.edu.az>
2023-04-29 14:46:07 -0700

    added file

---
+++ ASP.Net-intro/ASP.Net-intro/Controller/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net_intro.Controller
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            int[] nums = { 1, 2, 4, 5, 6, 7, 8 };
+            return View(nums);
+        }
+    }
+}
